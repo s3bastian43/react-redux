93bf3127a6d31466f196437a97015c03a5a78c22
SebastianC <ciobanu.sebastian96@yahoo.com>
2019-05-16 16:33:04 +0300

    Added React Router (store, actions, reducers). Added Mock API. Added Courses page. Manage courses page.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,13 +1,81 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
+import PropTypes from 'prop-types';
+import {bindActionCreators} from "redux";
+import CourseList from "./CourseList";
+import {Redirect} from 'react-router-dom';
+
 
 class CoursesPage extends Component {
+    state = {
+        redirectToAddCoursePAge: false
+    };
+
+    componentDidMount() {
+        const {courses, authors, actions} = this.props;
+        if (courses.length === 0) {
+            actions.loadCourses().catch(error => {
+                alert("Loading courses failed" + error);
+            });
+        }
+        if (authors.length === 0) {
+            actions.loadAuthors().catch(error => {
+                alert("Loading authors failed" + error);
+            })
+        }
+    }
+
     render() {
         return (
-            <h2>
-                Courses
-            </h2>
+            <>
+                {this.state.redirectToAddCoursePAge && <Redirect to="/course"/>}
+                <h2>
+                    Courses
+                </h2>
+                <button
+                    style={{marginBottom: 20}}
+                    className="btn btn-primary add-course"
+                    onClick={() => this.setState(({redirectToAddCoursePAge: true}))}
+                >
+                    Add Course
+                </button>
+                <CourseList courses={this.props.courses}/>
+            </>
         );
     }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    authors: PropTypes.array.isRequired,
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        courses:
+            state.authors.length === 0
+                ? []
+                : state.courses.map(course => {
+                    return {
+                        ...course,
+                        authorName: state.authors.find(a => a.id === course.authorId).name
+                    };
+                }),
+        authors: state.authors
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: {
+            loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+            loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+
+        }
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
